d0771739717b6f59b40fdc7d5c70de38a34058f5
kristonmcconnell <kriston.mcconnell@gmail.com>
2023-07-07 16:59:11 -0700

    uploading rolodex project to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import {Component} from 'react';
+
+import CardList from './components/card-list/CardList.component';
+import SearchBox from './components/search-box/SearchBox.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // this state is always a json object
+    this.state = {
+      monsters: [],
+      searchField: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      // whatever is returned from the above json will be passed into the users below
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      }
+      ))
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState(() => {
+      return {searchField}
+    })
+  }
+
+  render() {
+    // these two consts allow us to remove the 'this.state' from the state references
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+    // we know we have an array of monsters [ { name: 'Kriston' }, { name: 'Mash' } ]
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+    return (
+      <div className="App">
+        <h1 className='app-title'>MONSTERS</h1>
+        <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='monsters-search-box' />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
